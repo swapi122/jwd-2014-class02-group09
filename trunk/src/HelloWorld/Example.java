3aebe6c58f7a4799b786c7fb52ab3932f82a3f5f
11110045@student.hcmute.edu.vn <11110045@student.hcmute.edu.vn@fcf00cf0-5fd7-2be1-28c7-820555a816d3>
2014-09-09 14:29:22 +0000

    Update 9/9/2014

--- trunk/src/HelloWorld/Example.java
+++ trunk/src/HelloWorld/Example.java
@@ -4,7 +4,7 @@ public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hello World!!");
+System.out.print("Hello World!! This Group 09");
 	}
 
 }
