0175fc7e88932ce85c53fdffab006c7eef67e395
11110045 <11110045@student.hcmute.edu.vn>
2014-09-09 15:02:12 +0000

    Update 9/9/2014

--- src/Example09/ExampleHelloWorld.java
+++ src/Example09/ExampleHelloWorld.java
@@ -4,7 +4,7 @@ public class ExampleHelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hello World, This is my group.");
+     System.out.print("Hello World, Group:09.");
 	}
 
 }
